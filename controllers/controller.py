7fd8bb70b16e072c2ca75bc3d69c9993f5baaec9
alexandergm1 <alexander.1gmac@gmail.com>
2021-12-02 14:56:48 +0000

    MVP completed. Event planner created

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,8 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from models.list_of_events import *
 from models.event import Event
 
 @app.route('/events')
 def index():
+    return render_template('index.html', title="Home", events=events, headings=headings)
+
+
+@app.route('/events', methods=['POST'])
+def add_task():
+    date = request.form["date"]
+    name_of_event = request.form["name_of_event"]
+    number_of_guests = request.form["number_of_guests"]
+    room_location = request.form["room_location"]
+    description = request.form["description"]
+    new_event = Event(date, name_of_event, number_of_guests, room_location, description)
+    add_new_event(new_event)
     return render_template('index.html', title="Home", events=events, headings=headings)
--- models/list_of_events.py
+++ models/list_of_events.py
@@ -7,3 +7,6 @@ event3 = Event("02/08/2022", "CodeClan Bowling Night", 16, "Tenpin", "Bowling Ch
 events = [event1, event2, event3]
 
 headings = ["Date", "Name of Event", "Number of Guests", "Room Location", "Description"]
+
+def add_new_event(new_event):
+    events.append(new_event)
