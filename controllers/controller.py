38020ec31f645076c3e10d911fbad216d0ffcb14
alexandergm1 <alexander.1gmac@gmail.com>
2021-12-02 14:01:57 +0000

    Classes set up

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,6 @@
 from app import app
+from models.list_of_events import *
+from models.event import Event
 
 @app.route('/events')
 def index():
---
+++ models/list_of_events.py
@@ -0,0 +1,7 @@
+from models.event import *
+
+event1 = Event("27/12/2021", "CodeClan Marathon", 16, "Hopper", "Coding marathon for E54")
+event2 = Event("14/03/2022", "CodeClan Graduation", 16, "Pub", "Graduation for E54")
+event3 = Event("02/08/2022", "CodeClan Bowling Night", 16, "Tenpin", "Bowling Championship")
+
+events = [event1, event2, event3]
